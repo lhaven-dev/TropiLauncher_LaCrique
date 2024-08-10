5bbdba44d7d31f5d59e64ac572ff2426397f0803
Enzo <kdenzo88@gmail.com>
2024-08-10 15:45:22 +0200

    Remove Fancy menu for build server

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 exports.REMOTE_DISTRO_URL = 'https://files.tropimon.fr/distribution.json'
-print(exports.REMOTE_DISTRO_URL)
+
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
     null, // Injected forcefully by the preloader.
